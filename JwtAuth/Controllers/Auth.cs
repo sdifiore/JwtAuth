330f771f34678e3d5110dd69fafc30f278a08908
Sergio Di Fiore <difiores@outlook.com>
2025-06-08 08:43:24 -0300

    Add project files.

---
+++ JwtAuth/Controllers/Auth.cs
@@ -0,0 +1,29 @@
+﻿using JwtAuth.Entities;
+using JwtAuth.Models;
+
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace JwtAuth.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class Auth : ControllerBase
+    {
+        public static User user = new();
+
+        [HttpPost("register")]
+        public ActionResult<User> Register(UserDto request)
+        {
+            var hashedPassword = new PasswordHasher<User>()
+                .HashPassword(user, request.Password);
+
+            user.Username = request.Username;
+            user.PasswordHash = hashedPassword;
+
+            return Ok(user);
+
+            // This is a simple example.
+        }
+    }
+}
